193272c4f1e6da8f24eceff28c17f5a05e4b5bf2
klaoude <neoklaoude@gmail.com>
2020-02-05 13:47:29 +0100

    Add graphic and change requirement for pygame

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ Required package:
 - fswebcam
 - python 2.7
 - python-pip
+- noip DUC (for DNS) 
 
 Add user to video group: sudo usermod -a -G video <username>
 
 Install all python module required:
 pytnon -m pip install -r requirement.txt
 
+DNS:
+create an account on noip.com, configure the DUC and change URL_DNS in main.py
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import RPi.GPIO as GPIO
 import os
 import time
 import base64
 import socket
 import sys
 import qrcode
+import pygame
+from pygame.locals import *
 
 DEBUG = 0
+UPLOAD_DIR = "uploads/"
+QR_DIR = UPLOAD_DIR + "qr/"
 
-if sys.argv[1] == "-d":
+if len(sys.argv) > 1 and sys.argv[1] == "-d":
     DEBUG = 1
+else:
+    import RPi.GPIO as GPIO
 
-URL_DNS = ""
+URL_DNS = "localhost"
 URL_IP  = socket.gethostbyname(socket.gethostname())
 
 BUTTON_PIN = 12
@@ -23,16 +28,43 @@ if URL_DNS == "":
     print "DNS disable, IP = {}".format(URL_IP)
     WWW = URL_IP
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(BUTTON_PIN, GPIO.IN)
+if not DEBUG:
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(BUTTON_PIN, GPIO.IN)
 
-while True:
-    if DEBUG:
-        input()
-    else:
-        GPIO.wait_for_edge(BUTTON_PIN, GPIO.RISING)
-    
-    os.system("fswebcam --no-banner uploads/"+base64.b64encode(time.time())+".png")
-    url = "http://" + WWW + "/upload"
+def get_image_and_QR():
+    filename = base64.b64encode(str(time.time()))+".png"
+    os.system("fswebcam --no-banner " + UPLOAD_DIR + filename)
+    url = "http://" + WWW + "/" + UPLOAD_DIR + filename
     qr = qrcode.make(url)
-    print qr
+    qr.save(QR_DIR + filename, "PNG")
+    return filename
+
+def show_images_on_screen(window):
+    file = get_image_and_QR()
+    img = pygame.image.load(UPLOAD_DIR + file)
+    img_rect = img.get_rect()
+    qr = pygame.image.load(QR_DIR + file)
+    qr_rect = qr.get_rect()
+    window.blit(img, img_rect)
+    window.blit(qr, (400, 0))
+
+pygame.init()
+window = pygame.display.set_mode((800, 600))
+exit = 0
+
+while not exit:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit = 1
+        if DEBUG and event.type == KEYDOWN:
+            show_images_on_screen(window)
+        if DEBUG and event.type == MOUSEBUTTONDOWN:
+            print "pos = {}".format(event.pos)
+    
+    if not DEBUG:
+        GPIO.wait_for_edge(BUTTON_PIN, GPIO.RISING)
+        show_images_on_screen(window)
+
+    pygame.display.flip()
+    
